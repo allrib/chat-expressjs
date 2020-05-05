1149096e24b646cbdf2452048510ada5df6126fc
Alexis Ribot <alexisribot10@gmail.com>
2020-05-05 22:36:46 +0200

    add chat

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var ent = require('ent');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    socket.on('new_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('new_client', pseudo);
+    });
+
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+});
+
+server.listen(8080);
